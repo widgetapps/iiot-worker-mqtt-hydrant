26b243885d62a1fee356d8fd57a2039b427c17dc
Darryl Patterson <widgetapps@gmail.com>
2018-09-24 10:54:04 -0400

    Getting the battery reading working...

--- env/production.js
+++ env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
+    db: 'mongodb://10.240.162.13/one-platform?',
     dbOptions: { useNewUrlParser: true },
     mqttoptions: {
         clientId: 'worker_hydrant',
