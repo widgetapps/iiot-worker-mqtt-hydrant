4f5cf8e0bd98cde323aeaf213b61bb9556e32ff5
Darryl Patterson <widgetapps@gmail.com>
2018-09-24 10:20:26 -0400

    Getting the battery reading working...

--- env/all.js
+++ env/all.js
@@ -6,6 +6,7 @@ module.exports = {
         description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
         keywords: 'MongoDB, Express, AngularJS, Node.js'
     },
+    dbOptions: { useNewUrlParser: true },
     ip: process.env.IP || '127.0.0.1',
     mqtt: process.env.MQTT || 'mqtts://mqtt.terepac.one:8883',
     amqp: process.env.AMQP || 'amqp://localhost'
--- env/production.js
+++ env/production.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     db: 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
+    dbOptions: { useNewUrlParser: true },
     mqttoptions: {
         clientId: 'worker_hydrant',
         username: 'worker',
