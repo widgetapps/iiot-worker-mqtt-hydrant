8ab7597b688baaead8031a43fc949435198a546c
Darryl Patterson <widgetapps@gmail.com>
2018-09-27 17:03:12 -0400

    Updated to support new topics. Working on pressure event processing.

--- server.js
+++ server.js
@@ -26,6 +26,8 @@ mongoose.connect(config.db, config.dbOptions, function(err) {
 let client  = mqtt.connect(config.mqtt, config.mqttoptions);
 let amqp = require('amqplib').connect(config.amqp);
 
+let pressureEventBuffer = [];
+
 console.log('Started on IP ' + config.ip + '. NODE_ENV=' + process.env.NODE_ENV);
 
 client.on('error', function (error) {
@@ -62,26 +64,38 @@ client.on('message', function (topic, message) {
         switch (type) {
             case 'pressure':
                 data.sensorType = 1;
-                data.min = decoded.value;
-                data.max = decoded.value;
-                data.avg = decoded.value;
-                data.point = decoded.value;
+                data.min     = decoded.min;
+                data.max     = decoded.max;
+                data.avg     = decoded.avg;
+                data.point   = decoded.value;
+                data.samples = decoded.n;
                 handleData(amqp, data, deviceId);
                 break;
             case 'temperature':
                 data.sensorType = 2;
-                data.min = decoded.value;
-                data.max = decoded.value;
-                data.avg = decoded.value;
-                data.point = decoded.value;
+                data.min     = null;
+                data.max     = null;
+                data.avg     = null;
+                data.point   = decoded.value;
+                data.samples = null;
                 handleData(amqp, data, deviceId);
                 break;
             case 'battery':
                 data.sensorType = 4;
-                data.min = decoded.value;
-                data.max = decoded.value;
-                data.avg = decoded.value;
-                data.point = decoded.value;
+                data.min     = null;
+                data.max     = null;
+                data.avg     = null;
+                data.point   = decoded.value;
+                data.samples = null;
+                handleData(amqp, data, deviceId);
+                break;
+            case 'rssi':
+                data.sensorType = 10;
+                data.min     = decoded.min;
+                data.max     = decoded.max;
+                data.avg     = decoded.avg;
+                data.point   = decoded.value;
+                data.samples = decoded.n;
                 handleData(amqp, data, deviceId);
                 break;
             case 'reset':
@@ -91,10 +105,177 @@ client.on('message', function (topic, message) {
                 updateGeolocation(deviceId, decoded.latitude, decoded.longitude);
                 break;
             case 'pressure-event':
+                handlePressureEventData(amqp, deviceId, decoded);
                 break;
         }
     });
 });
+
+function handlePressureEventData(amqp, deviceId, data){
+    let date = new Date(data.date);
+    let key = date.getTime() + deviceId;
+
+    // Only one part, just process
+    if (data.part[0] === 1 && data.part[1] === 1) {
+        if (key in pressureEventBuffer) {
+            delete pressureEventBuffer[key];
+        }
+
+        pressureEventBuffer[key] = {
+            parts: data.parts[1],
+            date: data.date,
+            deviceId: deviceId,
+            values: data.value
+        };
+
+        queuePressureEventData(amqp, deviceId, key);
+
+        return;
+    }
+
+    // If this is the first part, create the array element
+    if (data.part[0] === 1) {
+        if (key in pressureEventBuffer) {
+            delete pressureEventBuffer[key];
+        }
+
+        pressureEventBuffer[key] = {
+            parts: data.parts[1],
+            date: data.date,
+            deviceId: deviceId,
+            values: data.value
+        };
+        return;
+    }
+
+    // If this is the last part, append and pub values
+    if (data.part[0] === pressureEventBuffer[key].parts) {
+
+        pressureEventBuffer[key].values.push(data.value);
+
+        queuePressureEventData(amqp, deviceId, key);
+
+        return;
+    }
+
+    // If this is a middle part, just append
+    if (data.part[0] < pressureEventBuffer[key].parts) {
+        pressureEventBuffer[key].values.push(data.values);
+        return;
+    }
+}
+
+function queuePressureEventData(amqp, deviceId, key) {
+    // No key, do nothing
+    if (!(key in pressureEventBuffer)) {
+        return;
+    }
+
+    Device.findOne({ serialNumber: deviceId })
+        .populate('client')
+        .exec(function (err, device) {
+            //console.log('Device queried: ' + deviceId);
+            if (!device || err) {
+                console.log('Device not found');
+                return;
+            }
+
+            amqp.then (function(conn) {
+                //console.log('AMQP connection established');
+                return conn.createChannel();
+            }).then (function(ch) {
+
+                let assetPromise = Asset.findById(device.asset).populate('location').exec();
+
+                assetPromise.then(function (asset) {
+                    let docPromise = buildPressureEventDocs(asset, device, key);
+
+                    docPromise.then(function (documents) {
+                        //console.log('Sending data to queue...');
+                        let q = 'telemetry';
+                        let ok = ch.assertQueue(q, {durable: true});
+                        return ok.then(function() {
+
+                            documents.forEach(function (document) {
+                                //ch.sendToQueue(q, new Buffer(JSON.stringify(document)), {persistent: true});
+                                console.log(JSON.stringify(document));
+                            });
+
+                            // Done processing, delete the key
+                            if (key in pressureEventBuffer) {
+                                delete pressureEventBuffer[key];
+                            }
+
+                            return ch.close();
+                        }).catch(console.warn);
+                    }).catch(console.warn);
+                }).catch(console.warn);
+
+            }).catch(console.warn);
+
+        });
+
+}
+
+function buildPressureEventDocs(asset, device, key) {
+
+    let promise = Sensor.findOne({ type: 1 }).exec();
+    return promise.then(function (sensor) {
+
+        let documents = [];
+        let document;
+
+        for (let i=0; i < pressureEventBuffer[key].values.length; i++) {
+
+            document = {
+                timestamp: pressureEventBuffer[key].date,
+                tag: {
+                    full: asset.location.tagCode + '_' + asset.tagCode + '_' + sensor.tagCode,
+                    clientTagCode: device.client.tagCode,
+                    locationTagCode: asset.location.tagCode,
+                    assetTagCode: asset.tagCode,
+                    sensorTagCode: sensor.tagCode
+                },
+                asset: {
+                    _id: asset._id,
+                    tagCode: asset.tagCode,
+                    name: asset.name,
+                    description: asset.description,
+                    location: {
+                        tagCode: asset.location.tagCode,
+                        description: asset.location.description,
+                        geolocation: asset.location.geolocation.coordinates
+                    }
+                },
+                device: {
+                    _id: device._id,
+                    serialNumber: device.serialNumber,
+                    type: device.type,
+                    description: device.description
+                },
+                sensor: {
+                    type: sensor.type,
+                    typeString: sensor.typeString,
+                    description: sensor.description,
+                    unit: sensor.unit
+                },
+                client: device.client._id,
+                data: {
+                    _id: sensor._id,
+                    unit: sensor.unit,
+                    values: {
+                        point: pressureEventBuffer[key].values[i]
+                    }
+                }
+            };
+
+            documents.push(document);
+        }
+
+        return documents;
+    });
+
+}
 
 function handleData(amqp, data, deviceId) {
     //console.log('Querying the deviceId ' + deviceId);
@@ -185,7 +366,8 @@ function buildMessage(asset, device, data, callback) {
                     min: data.min,
                     max: data.max,
                     average: data.avg,
-                    point: data.point
+                    point: data.point,
+                    samples: data.samples
                 }
             }
         };
