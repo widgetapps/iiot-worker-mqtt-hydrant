cb1f8c79c052dd170d2f5bd52186324fe65bc8ed
Darryl Patterson <widgetapps@gmail.com>
2019-01-09 14:47:36 -0500

    Debugging maultipart data...

--- env/production.js
+++ env/production.js
@@ -5,6 +5,7 @@ module.exports = {
     dbOptions: { useNewUrlParser: true },
     mqtt: process.env.MQTT || 'mqtts://10.240.0.16:8883',
     mqttoptions: {
+        clientId: 'worker_hydrant',
         username: 'worker',
         keepalive: 0,
         clean: false,
