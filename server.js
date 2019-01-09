e6fb0e0eb2112a12ae25d3b8235274bfe95233fa
Darryl Patterson <widgetapps@gmail.com>
2019-01-09 12:56:17 -0500

    Debugging maultipart data...

--- server.js
+++ server.js
@@ -151,7 +151,7 @@ function handlePartData(type, amqp, deviceId, data) {
     // Convert date back to milliseconds to create new date, just for creating the part key
     let date = new Date(timestamp / 1000);
     let key = date.getTime().toString() + deviceId;
-    console.log('BUILDING KEY FOR PART '+ data.part[0] + ' OF ' + data.part[1] + ': ' + date.getTime().toString() + ' ' + deviceId);
+    console.log('BUILDING KEY FOR PART '+ data.part[0] + ' OF ' + data.part[1] + ': ' + date.getTime().toString() + ' ' + deviceId + ' -- ' + key);
 
     // Only one part, just process
     if (data.part[0] === 1 && data.part[1] === 1) {
@@ -195,8 +195,8 @@ function handlePartData(type, amqp, deviceId, data) {
 
     if (typeof partBuffer[type][key] === 'undefined') {
         console.log('Parts array missing.');
-        //console.log('KEY: ', + key);
-        //console.log('PARTS: ' + data.part[0] + ':' + data.part[1]);
+        console.log('KEY: ', + key);
+        console.log('PARTS: ' + data.part[0] + ':' + data.part[1]);
         return;
     }
 
