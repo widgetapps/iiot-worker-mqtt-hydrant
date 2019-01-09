497562184488638238513e6c9a9d47e697a6d8bd
Darryl Patterson <widgetapps@gmail.com>
2019-01-09 11:05:39 -0500

    Debugging maultipart data...

--- server.js
+++ server.js
@@ -191,7 +191,7 @@ function handlePartData(type, amqp, deviceId, data) {
         return;
     }
 
-    if (typeof partBuffer[type][key].parts === 'undefined') {
+    if (typeof partBuffer[type][key] === 'undefined') {
         console.log('Parts array missing.');
         console.log('KEY: ', + key);
         console.log('PARTS: ' + data.part[0] + ':' + data.part[1]);
