1975c798350361d7ff1ae935dd39c4fbfb801976
Darryl Patterson <widgetapps@gmail.com>
2019-01-09 13:35:31 -0500

    Debugging maultipart data...

--- server.js
+++ server.js
@@ -37,17 +37,17 @@ client.on('error', function (error) {
 });
 
 client.on('connect', function () {
-   //  console.log('Connected to MQTT server: ' + config.mqtt);
+    console.log('Connected to MQTT server: ' + config.mqtt);
     // Subscribe to hydrant pubs
     client.subscribe(['+/v1/pressure', '+/v1/temperature', '+/v1/battery', '+/v1/reset', '+/v1/location', '+/v1/pressure-event', '+/v1/rssi', '+/v1/hydrophone'], {qos: 2});
 });
 
 client.on('reconnect', function () {
-   // console.log('Reconnecting to MQTT server...');
+   console.log('Reconnecting to MQTT server...');
 });
 
 client.on('close', function () {
-    // console.log('MQTT connection closed.');
+    console.log('MQTT connection closed.');
 });
 
 client.on('offline', function () {
