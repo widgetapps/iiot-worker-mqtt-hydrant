85e6dcef4d69552d47702a6af37eb22b65e0c37c
Darryl Patterson <widgetapps@gmail.com>
2019-01-13 13:53:47 -0500

    Testing new Redis implementation...

--- env/production.js
+++ env/production.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     db: 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
-    dbOptions: { useNewUrlParser: true,  useCreateIndex: true },
+    dbOptions: { useNewUrlParser: true,  useCreateIndex: true, useFindAndModify: false },
     mqtt: process.env.MQTT || 'mqtts://10.240.0.16:8883',
     mqttoptions: {
         clientId: 'worker_hydrant',
