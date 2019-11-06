a4dac27d638d96a57a945424798c2806c441d48c
Darryl Patterson <darryl.patterson@terepac.com>
2019-11-06 14:53:23 -0500

    Clean false

--- env/production.js
+++ env/production.js
@@ -8,7 +8,7 @@ module.exports = {
         clientId: 'worker_hydrant',
         username: 'worker',
         keepalive: 60,
-        clean: true,
+        clean: false,
         password: process.env.MQTT_PASSWORD || ''
     }
 };
