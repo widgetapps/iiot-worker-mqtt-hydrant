dc3765ac9521227c38bd8e950ba49c0368b2fe62
Darryl Patterson <darryl.patterson@terepac.com>
2019-11-14 17:31:54 -0500

    Updated config.

--- env/production.js
+++ env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
+    db: process.env.MONGO_STRING || 'mongodb://10.240.162.13,10.240.253.155/one-platform?replicaSet=rs0',
     dbOptions: { useNewUrlParser: true,  useCreateIndex: true, useFindAndModify: false },
     mqtt: process.env.MQTT || 'mqtts://10.240.0.16:8883',
     mqttoptions: {
